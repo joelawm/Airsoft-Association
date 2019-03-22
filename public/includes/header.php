a77e3270ec04b8884084ede18381da1c9d2082d4
jlmeyer <xdeltagix@gmail.com>
2019-03-22 13:57:15 -0500

    no real fixes

--- public/includes/header.php
+++ public/includes/header.php
@@ -12,7 +12,7 @@
           <div class="container">
                <div class="navbar-header">
                     <a class="navbar-brand" href="/"><b>NAA</b></a>
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbarCollapse">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarCollapse" aria-expanded="false">
                        <span class="sr-only">Toggle navigation</span>
                        <span class="icon-bar"></span>
                        <span class="icon-bar"></span>
@@ -20,37 +20,39 @@
                     </button>
                </div>
                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="nav navbar-nav navbar-right">
-                         <li><a href="/Sponsors">SPONSORS</a></li>
-                         <li><a href="/Forum">FORUM</a></li>
-                         <li class="dropdown">
-                              <a href="/Information" class="dropdown-toggle" data-toggle="dropdown">INFORMATION<span class="caret"></span></a>
-                              <ul class="dropdown-menu">
-                                   <li><a href="/Information/PlacesToAirsoft">Places to Airsoft</a></li>
-                                   <li><a href="/Information/GeneralRules">General Rules</a></li>
-                                   <li><a href="/Information/AirsoftStores">Airsoft Stores</a></li>
-                              </ul>
-                         </li>
-                         <li><a href="/Media">MEDIA</a></li>
-                         <li><a href="/Donate">DONATE</a></li>
-                         <li><a href="/Contact">CONTACT</a></li>
-                         <li><a class="hidden-xs" style="font-size: 20px;">|</a></li>
-                         <?php
-                         // Initialize the session
-                         if(!isset($_SESSION))
-                         {
-                              session_start();
-                         }
+                    <div class="clearfix">
+                         <ul class="nav navbar-nav navbar-right">
+                              <li><a href="/Sponsors">SPONSORS</a></li>
+                              <li><a href="/Forum">FORUM</a></li>
+                              <li class="dropdown">
+                                   <a href="/Information" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">INFORMATION<span class="caret"></span></a>
+                                   <ul class="dropdown-menu">
+                                        <li><a href="/Information/PlacesToAirsoft">Places to Airsoft</a></li>
+                                        <li><a href="/Information/GeneralRules">General Rules</a></li>
+                                        <li><a href="/Information/AirsoftStores">Airsoft Stores</a></li>
+                                   </ul>
+                              </li>
+                              <li><a href="/Media">MEDIA</a></li>
+                              <li><a href="/Donate">DONATE</a></li>
+                              <li><a href="/Contact">CONTACT</a></li>
+                              <li><a class="hidden-xs" style="font-size: 20px;">|</a></li>
+                              <?php
+                              // Initialize the session
+                              if(!isset($_SESSION))
+                              {
+                                   session_start();
+                              }
 
-                         // Check if the user is already logged in, if yes then redirect him to welcome page
-                         if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) : ?>
-                              <li><a href="/account.php">ACCOUNT</a></li>
-                              <li><a href="/logout.php">SIGN OUT</a></li>
-                         <?php else : ?>
-                              <li><a href="/login.php">LOGIN</a></li>
-                              <li><a href="/r.php">SIGN UP</a></li>
-                         <?php endif; ?>
-                    </ul>
+                              // Check if the user is already logged in, if yes then redirect him to welcome page
+                              if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) : ?>
+                                   <li><a href="/account.php">ACCOUNT</a></li>
+                                   <li><a href="/logout.php">SIGN OUT</a></li>
+                              <?php else : ?>
+                                   <li><a href="/login.php">LOGIN</a></li>
+                                   <li><a href="/r.php">SIGN UP</a></li>
+                              <?php endif; ?>
+                         </ul>
+                    </div>
                </div>
           </div>
      </nav>
