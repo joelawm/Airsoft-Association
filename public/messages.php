10bd20f33914348856d4bbd763c0c02fb2663356
jlmeyer <xdeltagix@gmail.com>
2019-03-25 01:08:22 -0500

    All links lead to somewhere

---
+++ public/messages.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+     <title>Media</title>
+     <?php include($_SERVER['DOCUMENT_ROOT'] . "/includes/head.txt"); ?>
+</head>
+<body>
+     <?php include($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"); ?>
+     <div class="main">
+          <div class="text-center">
+               <b style="font-size:60px;color:#000000;">Media</b>
+          </div>
+          <div class="row">
+               <p class="para center-block text-center" style="font-size:15px;">
+               The video of the week is based on a video we think everyone should get a chance to see! We don't pick favorites and it might
+               give you some new content to binge watch before the next airsoft game!</p>
+          </div>
+          <div class="row center-block text-center para">
+               <p class="para" style="font-size:15px;">
+               I do plan on doing a complete overhaul of this section but for now here you guys go!</p>
+          </div>
+          <div class="row text-center">
+               <b style="font-size:30px;color:#000000;">Video of the week!</b>
+          </div>
+          <div class="row">
+               <center><iframe width="900" height="450" src="https://www.youtube.com/embed/dvRPisg6CO0"></iframe></center>
+          </div>
+     </div>
+</body>
+</html>
